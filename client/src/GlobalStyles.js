5da8553602ec90b3281555d7700b96bd74d695e4
lvest <ookkid.1@gmail.com>
2022-01-19 23:50:45 +0900

    Add GlobalStyles

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -3,6 +3,16 @@ import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
+  
+  * {
+    box-sizing: border-box;
+  }
+
+  main {
+    margin-top: 50px;
+    margin-bottom: 60px;
+  }
+  
 `;
 
 export default GlobalStyles;
